301b27533c3f65ca6f69e0d09d3641e282496cf0
Israel Nizama <mnizama@baufest.com>
2022-06-01 10:09:41 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Container,
+  Icon,
+  Image,
+  Menu,
+  Segment,
+  Sidebar,
+} from "semantic-ui-react";
+import { useMediaQuery } from "react-responsive";
+import { useState } from "react";
+import "./App.css";
+import Feature from "./Feature";
 
 function App() {
+  const [sidebarOpened, setSidebarOpened] = useState(false);
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const handleSidebarHide = () => setSidebarOpened(false);
+  const handleToggle = () => setSidebarOpened(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isTabletOrMobile && (
+        <Sidebar.Pushable>
+          <Sidebar
+            as={Menu}
+            animation="overlay"
+            inverted
+            onHide={handleSidebarHide}
+            vertical
+            visible={sidebarOpened}
+          >
+            <Menu.Item as="a" header>
+              <Image
+                size="mini"
+                src="/logo192.png"
+                style={{ marginRight: "1.5em" }}
+              />
+              Project Name
+            </Menu.Item>
+            <Menu.Item as="a" active>
+              Home
+            </Menu.Item>
+            <Menu.Item as="a">Work</Menu.Item>
+            <Menu.Item as="a">Company</Menu.Item>
+            <Menu.Item as="a">Careers</Menu.Item>
+            <Menu.Item as="a">Log in</Menu.Item>
+            <Menu.Item as="a">Sign Up</Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher dimmed={sidebarOpened}>
+            <Menu inverted>
+              <Menu.Item onClick={handleToggle}>
+                <Icon name="sidebar" />
+              </Menu.Item>
+              <Menu.Item position="right">
+                <Button as="a" inverted>
+                  Log in
+                </Button>
+                <Button as="a" inverted style={{ marginLeft: "0.5em" }}>
+                  Sign Up
+                </Button>
+              </Menu.Item>
+            </Menu>
+            <Container text style={{ marginTop: "2em" }}>
+              <Feature />
+            </Container>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      )}
+      {isDesktopOrLaptop && (
+        <>
+          <Menu fixed="top" inverted>
+            <Container>
+              <Menu.Item as="a" header>
+                <Image
+                  size="mini"
+                  src="/logo192.png"
+                  style={{ marginRight: "1.5em" }}
+                />
+                Project Name
+              </Menu.Item>
+              <Menu.Item as="a" active>
+                Home
+              </Menu.Item>
+              <Menu.Item as="a">Work</Menu.Item>
+              <Menu.Item as="a">Company</Menu.Item>
+              <Menu.Item as="a">Careers</Menu.Item>
+              <Menu.Item as="a">Log in</Menu.Item>
+              <Menu.Item as="a">Sign Up</Menu.Item>
+              <Menu.Item position="right">
+                <Button as="a" inverted>
+                  Log in
+                </Button>
+                <Button as="a" inverted style={{ marginLeft: "0.5em" }}>
+                  Sign Up
+                </Button>
+              </Menu.Item>
+            </Container>
+          </Menu>
+          <Container text style={{ marginTop: "6em" }}>
+            <Feature />
+          </Container>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/Feature.js
@@ -0,0 +1,45 @@
+import { Container, Header, Image } from "semantic-ui-react";
+
+function Feature() {
+  return (
+    <>
+      <Header as="h1">Semantic UI React Fixed Template</Header>
+      <p>This is a basic fixed menu template using fixed size containers.</p>
+      <p>
+        A text container is used for the main container, which is useful for
+        single column layouts.
+      </p>
+
+      <Image
+        src="/media-paragraph.png"
+        style={{ marginTop: "2em" }}
+      />
+      <Image
+        src="/paragraph.png"
+        style={{ marginTop: "2em" }}
+      />
+      <Image
+        src="/paragraph.png"
+        style={{ marginTop: "2em" }}
+      />
+      <Image
+        src="/paragraph.png"
+        style={{ marginTop: "2em" }}
+      />
+      <Image
+        src="/paragraph.png"
+        style={{ marginTop: "2em" }}
+      />
+      <Image
+        src="/paragraph.png"
+        style={{ marginTop: "2em" }}
+      />
+      <Image
+        src="/paragraph.png"
+        style={{ marginTop: "2em" }}
+      />
+    </>
+  );
+}
+
+export default Feature;
